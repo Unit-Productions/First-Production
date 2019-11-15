74d4ca3ff46bf17b7b5c4a99931fe56ed1ebfdda
adam1279 <adamdamgolan@gmail.com>
2019-11-15 21:00:33 +0100

    changes

--- main/script.js
+++ main/script.js
@@ -12,3 +12,17 @@ function pressPlay(t) {
             t.children[0].src = 'icons/Play.png';
     }
 }
+function metroPress(t) {
+    var bool2 = 'false';
+    if (t.getAttribute('data-state')=='false') {
+        bool2 = 'true';
+    }
+    t.setAttribute('data-state', bool2);
+    switch (bool2) {
+        case 'true':
+            t.children[0].src = 'icons/Metronome.png';
+            break;
+        case 'false':
+            t.children[0].src = 'icons/Metronome_off.png';
+    }
+}
